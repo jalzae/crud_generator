07ce94024aab97634cb1937771597898e05440a5
jalzae <jalzae@gmail.com>
2021-08-31 11:20:05 +0700

    fixing show tables

--- showtable.php
+++ showtable.php
@@ -1,14 +1,14 @@
 <?php
 $dbnya = $_POST["data"];
 $success = new mysqli("localhost", "root", "", "$dbnya");
-$set = mysqli_query($success, "SHOW TABLES;");
+$set = mysqli_query($success, "SHOW TABLES IN $dbnya;");
 $dbs = array();
 $kabehdb = array();
 while ($db = mysqli_fetch_row($set))
     $dbs[] = $db[0];
 
-    $hasil = count($dbs);
-    for ($i = 0; $i < $hasil; $i++) {
-        print_r("<option value='$dbs[$i]'>" . $dbs[$i] . "</option>");
-    }
-    mysqli_close($success);
+$hasil = count($dbs);
+for ($i = 0; $i < $hasil; $i++) {
+    print_r("<option value='$dbs[$i]'>" . $dbs[$i] . "</option>");
+}
+mysqli_close($success);
